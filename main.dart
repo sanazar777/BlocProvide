05607ebf85b855eef9b70d233537082a3ac86b59
sanazar777 <sanazars.ss@gmail.com>
2025-04-02 22:24:48 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,47 @@
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_bloc_project/counter_bloc.dart';
+import 'dart:async';
+
+// Cubit State
+abstract class ListState {}
+
+class ListInitial extends ListState {}
+
+class ListLoading extends ListState {}
+
+class ListLoaded extends ListState {
+  final List<String> items;
+  ListLoaded(this.items);
+}
+
+// Cubit
+class ListCubit extends Cubit<ListState> {
+  ListCubit() : super(ListInitial());
+
+  Future<void> loadItems() async {
+    emit(ListLoading());
+    await Future.delayed(Duration(seconds: 2)); // Fake API call
+    emit(ListLoaded(["Item 1", "Item 2", "Item 3"]));
+  }
+
+  void addItem(String item) {
+    if (state is ListLoaded) {
+      final items = List<String>.from((state as ListLoaded).items)..add(item);
+      emit(ListLoaded(items));
+    }
+  }
+
+  void removeItem(int index) {
+    if (state is ListLoaded) {
+      final items = List<String>.from((state as ListLoaded).items);
+      if (index >= 0 && index < items.length) {
+        items.removeAt(index);
+        emit(ListLoaded(items));
+      }
+    }
+  }
+}
 
 void main() {
   runApp(MyApp());
@@ -11,47 +52,72 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
+    return BlocProvider(
+      create: (context) => ListCubit()..loadItems(),
+      child: MaterialApp(home: ListPage()),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class ListPage extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+
+  ListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterBloc>(
-      create: (context) => CounterBloc()..add(CounterIncEvent()),
-      child: BlocBuilder<CounterBloc, int>(builder: (context, state) {
-        final bloc = BlocProvider.of<CounterBloc>(context);
-        return Scaffold(
-          floatingActionButton: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                  bloc.add(CounterIncEvent());
-                },
-                icon: Icon(Icons.plus_one),
-              ),
-              IconButton(
-                onPressed: () {
-                  bloc.add(CounterDecEvent());
-                },
-                icon: Icon(Icons.exposure_minus_1),
-              )
-            ],
-          ),
-          body: Center(
-            child: Text(
-              state.toString(),
-              style: TextStyle(fontSize: 33),
+    return Scaffold(
+      appBar: AppBar(title: Text('Advanced Cubit Example')),
+      body: Column(
+        children: [
+          Expanded(
+            child: BlocBuilder<ListCubit, ListState>(
+              builder: (context, state) {
+                if (state is ListLoading) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is ListLoaded) {
+                  return ListView.builder(
+                    itemCount: state.items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(state.items[index]),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed:
+                              () => context.read<ListCubit>().removeItem(index),
+                        ),
+                      );
+                    },
+                  );
+                }
+                return Center(child: Text('Press button to load items'));
+              },
             ),
           ),
-        );
-      }),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(labelText: 'Enter item'),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    if (_controller.text.isNotEmpty) {
+                      context.read<ListCubit>().addItem(_controller.text);
+                      _controller.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
